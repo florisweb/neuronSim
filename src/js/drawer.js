07a58739ddddf8c636bc5411fffe34f6ae5bdb77
florisweb <floris@florisweb.dev>
2021-11-28 11:39:53 +0100

    Started implementing actual physics and chemistry plus made the ions dynamic.

--- src/js/drawer.js
+++ src/js/drawer.js
@@ -16,6 +16,7 @@ function _Drawer() {
 
   this.draw = function() {
     ctx.clearRect(0, 0, Canvas.width, Canvas.height);
+
     for (let x = 0; x < World.grid.width; x++)
     {
       for (let y = 0; y < World.grid.height; y++)
@@ -23,11 +24,17 @@ function _Drawer() {
         drawGridSquare(World.grid[x][y]);
       }
     }
+
+    ctx.fillStyle = '#fff';
+    ctx.font = '20px serif';
+    ctx.fillText(World.calcMembranePotential() + 'V', 10, 20);
+    ctx.fill();
+
     requestAnimationFrame(Drawer.draw);
   }
 
   function drawGridSquare(_square) {
-    ctx.fillStyle = 'rgba(' + _square.Cna * 255 + ', ' + _square.Ck * 255 + ', 0, .5)';
+    ctx.fillStyle = 'rgba(' + _square.concentrations[0] * 255 + ', ' + _square.concentrations[1] * 255 + ', ' + _square.concentrations[2] * 255 + ', .5)';
     if (_square.type == 1) ctx.fillStyle = '#aaa';
     if (_square.type == 2) ctx.strokeStyle = '#f00';
     if (_square.type == 3) ctx.strokeStyle = '#0f0';
@@ -37,6 +44,18 @@ function _Drawer() {
     if (_square.type <= 1)
     {
       ctx.fillRect(_square.x * squareWidth, _square.y * squareHeight, squareWidth, squareHeight);
+      ctx.fillText(
+        Math.round(_square.concentrations[0] * 100) / 100, 
+        _square.x * squareWidth, 
+        _square.y * squareHeight + squareHeight * .5
+      );
+      ctx.fillText(
+        Math.round(_square.calcVoltage({concentrations: [1, 1, 1]}) * 1000) / 1000 + 'V', 
+        _square.x * squareWidth, 
+        _square.y * squareHeight + squareHeight * .75
+      );
+
+      
     } else {
       if (_square.closed) ctx.fillStyle = ctx.strokeStyle;
       ctx.rect(_square.x * squareWidth, _square.y * squareHeight, squareWidth, squareHeight);
--- src/js/world.js
+++ src/js/world.js
@@ -1,10 +1,44 @@
-let diffusionConstant = .2;
-let eStaticConstant = .1;
-let NaKPumpSpeedConstant = .05;
+const Constants = {
+  e: 1.602 * Math.pow(10, -19),
+  fiscosity: 1,
+  particleRadius: Math.pow(10, -15), //-10
+  boltzman: 1.3807 * Math.pow(10, -23),
+  avogadro: 6.0221 * Math.pow(10, 23),
+  gas: 8.314,
+  faraday: 96485,
+  temperature: 20 + 273.15,
+};
+
+const Ions = [
+  {
+    name: 'Na+',
+    charge: 1,
+  },
+  {
+    name: 'K+',
+    charge: 1,
+  },
+  {
+    name: 'Cl-',
+    charge: -1,
+  }
+];
+
+
+
+let diffusionConstant = .001;
+let NaKPumpSpeedConstant = .1;
+let NaChangeVoltage = -.3;
 
 function _World() {
-  this.grid = new Grid(50, 20);
-  this.speed = 0;
+  this.size = { // m
+    width: 5 * Math.pow(10, -5),
+    height: 2 * Math.pow(10, -5),
+  }
+
+  this.grid = new Grid(5, 2, this);
+  this.speed = 1;
+  this.FPS = 60;
   this.updates = 0;
   
 
@@ -14,13 +48,18 @@ function _World() {
   }
 
   this.update = function() {
+    let dt = 1 / World.FPS * World.speed; // s
+    // for (let i = 0; i < 10; i++)
+    // {
     this.updates++;
-    this.grid.diffuse();
+    this.grid.diffuse(dt);
     this.grid.update();
-    setTimeout(() => {World.update()}, World.speed);
+    // }
+    setTimeout(() => {World.update()}, 1000 / World.FPS);
   }
 
   this.calcMembranePotentialAtX = function(_x) {
+    return 0;
     let chargeCytoplasm = this.grid[_x][membraneY + 1].Cna + this.grid[_x][membraneY + 1].Ck;
     let chargeExtracellular = this.grid[_x][membraneY - 1].Cna + this.grid[_x][membraneY - 1].Ck;
 
@@ -34,10 +73,12 @@ function _World() {
 }
 
 const membraneY = 10;
-function Grid(_width, _height) {
+function Grid(_width, _height, _parent) {
   let Self = [];
   Self.width = _width;
   Self.height = _height;
+  Self.tileWidth = _parent.size.width / _width; // m
+  Self.tileHeight = _parent.size.height / _height;
 
   for (let x = 0; x < Self.width; x++)
   {
@@ -45,25 +86,35 @@ function Grid(_width, _height) {
     for (let y = 0; y < Self.height; y++)
     {
       Self[x][y] = new GridSquare(x, y);
-      Self[x][y].Cna = .1;
-      Self[x][y].Ck = .1;
+      Self[x][y].concentrations = [Math.random(), Math.random(), 0];
 
 
-      if (y == membraneY) 
-      {
-        Self[x][y].type = 1;
-        // if (x % 4 == 0) Self[x][y].type = 2;
-        // if (x % 4 == 2) Self[x][y].type = 3;
-        // if (x % 4 == 2) Self[x][y].type = 3;
-        // if (Math.random() > .8) Self[x][y].type = 4;
+      // if (y == membraneY) 
+      // {
+      //   Self[x][y].type = 1;
+      //   // if (x % 4 == 0) Self[x][y].type = 2;
+      //   // if (x % 4 == 2) Self[x][y].type = 3;
+      //   // if (x % 4 == 2) Self[x][y].type = 3;
+      //   // if (Math.random() > .8) Self[x][y].type = 4;
 
-        // Self[x][y].closed = true; //Math.random() > .5;
-        if (x == 0) Self[x][y].type = 4;
-        if (x > 20) Self[x][y].type = 2;
-        if (x == 46) Self[x][y].type = 3;
+      //   // Self[x][y].closed = true; //Math.random() > .5;
+      //   if (x == 0) Self[x][y].type = 4;
+      //   if (x > 10) Self[x][y].type = 3;
+      //   if (x > 30) Self[x][y].type = 2;
 
-      }
-      if (y > membraneY) Self[x][y].isCytoplasm = true;
+      //   if (x == 5) 
+      //   {
+      //     Self[x][y].type = 3;
+      //     Self[x][y].closed = false;
+      //     Self[x][y].isLeakChannel = true;
+      //   }
+      // }
+      // if (y > membraneY) 
+      // {
+      //   Self[x][y].isCytoplasm = true;
+      // } else {
+      //   // Self[x][y].Cna = .9;
+      // }
     }
   } 
 
@@ -79,7 +130,7 @@ function Grid(_width, _height) {
   }
 
 
-  Self.diffuse = function() {
+  Self.diffuse = function(_dt) {
     // Calculate changes
     // for (let y = 0; y < Self.height; y++)
     // {
@@ -92,7 +143,7 @@ function Grid(_width, _height) {
     {
       for (let x = 0; x < Self.width; x++)
       {
-        Self[x][y].calcDiffusionDeltas();
+        Self[x][y].calcDiffusionDeltas(_dt);
       }
     } 
     // Apply changes
@@ -138,6 +189,7 @@ function GridSquare(x, y) {
   this.type = 0;
   this.closed = true;
   this.isCytoplasm = false;
+  this.isLeakChannel = false;
   // 0 = empty
   // 1 = cell-membrane
   // 2 = Na-channel?
@@ -145,24 +197,43 @@ function GridSquare(x, y) {
   // 4 = Na/K-pump
 
 
+  this.concentrations = [0, 0, 0];
+  this.diffusabilityFunctions = [
+    () => {
+      return This.type == 0 || (This.type == 2 && !This.closed);
+    },
+    () => {
+      return This.type == 0 || (This.type == 3 && !This.closed);
+    },
+    () => {
+      return x != 1;
+    }
+  ];
 
-  this.Cna = 0;
-  this.Ck = 0;
-  
-  this.dCna = 0;
-  this.dCk = 0;
-
-  this.NaDiffusable = function() {
-    return this.type == 0 || (this.type == 2 && !this.closed);
-  }
-  this.KDiffusable = function() {
-    return this.type == 0 || (this.type == 3 && !this.closed);
-  }
-
+  let changeOffset = Math.round(1000 * Math.random() / 10) * 10;
+  const naChannelUpdateLength = 10000;
+  let openedUpdateIndex = -naChannelUpdateLength;
+  let refractoryUpdateIndex = -naChannelUpdateLength;
   this.update = function() {
-    if (this.type == 2 && World.updates % 1000 == 0) // only updates every 100 updates
+    if (this.type == 2 && (World.updates - changeOffset) % 1000 == 0) // only updates every 1000 updates
     {
-      this.closed = World.calcMembranePotentialAtX(this.x) > -.2;
+      let curVoltage = World.calcMembranePotentialAtX(this.x);
+      if (openedUpdateIndex == World.updates - naChannelUpdateLength)
+      {
+        this.closed = true;
+        refractoryUpdateIndex = World.updates;
+      }
+
+      if (curVoltage > NaChangeVoltage && this.closed && refractoryUpdateIndex < World.updates - naChannelUpdateLength)
+      {
+        this.closed = false;
+        openedUpdateIndex = World.updates;
+      } 
+    }
+    if (this.type == 3 && !this.isLeakChannel && (World.updates - changeOffset) % 10000 == 0) // only updates every 1000 updates
+    {
+      let curVoltage = World.calcMembranePotentialAtX(this.x);
+      this.closed = curVoltage < NaChangeVoltage;
     };
 
 
@@ -171,46 +242,53 @@ function GridSquare(x, y) {
     if (World.grid[this.x][this.y - 1].Ck < 2 * dCUnit) dCUnit = World.grid[this.x][this.y - 1].Ck / 2;
     if (World.grid[this.x][this.y + 1].Cna < 3 * dCUnit) 
     {
-      let newDCUnit = World.grid[this.x][this.y + 1].Ck / 3;
+      let newDCUnit = World.grid[this.x][this.y + 1].Cna / 3;
       if (newDCUnit < dCUnit) dCUnit = newDCUnit;
     }
 
 
-    World.grid[this.x][this.y - 1].Cna  += 3 * dCUnit;
-    World.grid[this.x][this.y - 1].Ck   -= 2 * dCUnit;
-    World.grid[this.x][this.y + 1].Cna  -= 3 * dCUnit;
-    World.grid[this.x][this.y + 1].Ck   += 2 * dCUnit;
+    World.grid[this.x][this.y - 1].concentrations[0] += 3 * dCUnit;
+    World.grid[this.x][this.y - 1].concentrations[1] -= 2 * dCUnit;
+    World.grid[this.x][this.y + 1].concentrations[0] -= 3 * dCUnit;
+    World.grid[this.x][this.y + 1].concentrations[1] += 2 * dCUnit;
   }
 
 
-  this.calcDiffusionDeltas = function() {
+  this.calcVoltage = function(_other) {
+    let sumSelf = 0;
+    for (let ionConcentration of this.concentrations) sumSelf += ionConcentration;
+    
+    let sumOther = 0;
+    for (let ionConcentration of _other.concentrations) sumOther += ionConcentration;
+    if (sumOther == 0) sumOther = .00001;
+
+    let ionRatio = sumSelf / sumOther;
+    return Constants.gas * Constants.temperature / Constants.faraday * Math.log(ionRatio);
+  }
+
+  this.calcDiffusionDeltas = function(_dt) {
     let neighbours = getNeighbours();
 
-    if (This.NaDiffusable()) 
+    let dragCoefficient = 6 * Math.PI * Constants.fiscosity * Constants.particleRadius;
+    let D = Constants.boltzman * Constants.temperature / dragCoefficient;
+
+    for (let i = 0; i < Ions.length; i++)
     {
+      if (!this.diffusabilityFunctions[i]()) continue;
+
       for (let neighbour of neighbours)
       {
-        if (!neighbour.NaDiffusable()) continue;
-        let dEStatic = this.Cna + this.Ck - neighbour.Cna - neighbour.Ck;
+        if (!neighbour.diffusabilityFunctions[i]()) continue;
 
-        let dC = this.Cna - neighbour.Cna;
-        let dIons = (dC * diffusionConstant + dEStatic * eStaticConstant) * .5;
+        let V = this.calcVoltage(neighbour); 
+        let eField = V / World.grid.tileWidth; // l = 1
+        let dC = neighbour.concentrations[i] - this.concentrations[i]; // M
 
-        this.Cna      -= dIons;
-        neighbour.Cna += dIons;
-      }
-    }
-    if (This.KDiffusable()) 
-    {
-      for (let neighbour of neighbours)
-      {
-        if (!neighbour.KDiffusable()) continue;
-        let dEStatic = this.Cna + this.Ck - neighbour.Cna - neighbour.Ck;
-        let dC = this.Ck - neighbour.Ck;
-        let dIons = (dC * diffusionConstant + dEStatic * eStaticConstant) * .5;
+        let totalFlux = Ions[i].charge * Constants.e / dragCoefficient * this.concentrations[i] * eField - D * (dC / World.grid.tileWidth);
+        let dIonsMolair = totalFlux * _dt; // A = l^2 = 1
         
-        this.Ck      -= dIons;
-        neighbour.Ck += dIons;
+        this.concentrations[i]      -= dIonsMolair;
+        neighbour.concentrations[i] += dIonsMolair;
       }
     }
   }
